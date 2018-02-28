97fe58005b57de7c7cfecd5450861be206ac42b9
Cemozerr <cemozer2018@u.northwestern.edu>
2018-02-28 16:34:43 -0600

    Make event listening dynamic

--- listenerScript.js
+++ listenerScript.js
@@ -5,11 +5,13 @@ var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545"));
 
 // use hardcoded contract build & address
-var abi = require("./build/contracts/ClientReceipt").abi;
+var build = require("./build/contracts/ClientReceipt");
+var abi = build.abi;
 var ClientReceipt = web3.eth.contract(abi);
-var clientReceipt = ClientReceipt.at("0x345ca3e014aaf5dca488057592ee47305d9b3e10");
+var clientReceipt = ClientReceipt.at(build.networks['5777'].address);
 
-var event = clientReceipt.Deposit();
+var eventName = process.argv[2];
+var event = eval('clientReceipt.' + eventName + '()');
 
 // watch for event emissions
 event.watch(function(error, result){
