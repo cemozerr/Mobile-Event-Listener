a5bf328b1a3757a04e7cef75ebbe52c446c81e5e
Cemozerr <cemozer2018@u.northwestern.edu>
2018-02-28 16:23:12 -0600

    Have two test scripts to test for different events

--- testDeposit.js
+++ testDeposit.js
@@ -4,8 +4,9 @@ var web3 = new Web3(new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545"));
 
 // use hardcoded contract build & address
 var abi = require("./build/contracts/ClientReceipt").abi;
+var abi = build.abi;
 var ClientReceipt = web3.eth.contract(abi);
-var clientReceipt = ClientReceipt.at("0x345ca3e014aaf5dca488057592ee47305d9b3e10");
+var clientReceipt = ClientReceipt.at(build.networks['5777'].address);
 
 clientReceipt.deposit(web3.toHex({test:'test'}),{from:'0x627306090abaB3A6e1400e9345bC60c78a8BEf57', value:'1000000000000000000'})
 
